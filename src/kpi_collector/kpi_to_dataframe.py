44eda1e9bfc90881edb44d41ecf7f23103f0285a
Anselm Hahn <Anselm.Hahn@gmail.com>
2020-11-24 21:24:17 +0100

    yes

--- src/kpi_collector/kpi_to_dataframe.py
+++ src/kpi_collector/kpi_to_dataframe.py
@@ -2,7 +2,7 @@ import pytrends
 from pytrends.request import TrendReq
 # Only need to run this once, the rest of requests will use the same session.
 
-df main():
+def main():
     pytrend = TrendReq()
     pytrend.build_payload(["Blockchain", "Apple","S&P500"], timeframe='today 5-y')
     df = pytrend.interest_over_time()
