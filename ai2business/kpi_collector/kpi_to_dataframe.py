db4e4b37022911de346517674833719a92764c1e
Anselm Hahn <Anselm.Hahn@gmail.com>
2020-11-25 21:15:26 +0100

    Small resetup

--- ai2business/kpi_collector/kpi_to_dataframe.py
+++ ai2business/kpi_collector/kpi_to_dataframe.py
@@ -1,13 +1,16 @@
 import pytrends
 from pytrends.request import TrendReq
+
 # Only need to run this once, the rest of requests will use the same session.
+
 
 def main():
     pytrend = TrendReq()
-    pytrend.build_payload(["Blockchain", "Apple","S&P500"], timeframe='today 5-y')
+    pytrend.build_payload(["Blockchain", "Apple", "S&P500"], timeframe="today 5-y")
     df = pytrend.interest_over_time()
-    #df.plot()
-    print(df['Blockchain'].values)
+    # df.plot()
+    print(df["Blockchain"].values)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
