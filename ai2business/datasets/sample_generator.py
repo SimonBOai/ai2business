cdc5678788a11bb8479a5476730486ad2f86fe0f
Anselm Hahn <Anselm.Hahn@gmail.com>
2020-12-07 18:58:10 +0100

    Rebase from json to python

--- ai2business/datasets/sample_generator.py
+++ ai2business/datasets/sample_generator.py
@@ -1,22 +1,5 @@
 """Generates sample list of KPIs"""
-__all__ = ["stock_market"]
-import json
-from pathlib import Path
-
-__cwd__ = Path("ai2business/datasets/data/")
-
-
-def load_json(database: str) -> dict:
-    """Loading the database as JSON-files.
-
-    Args:
-        database (str): Name of the `JSON`-database.
-
-    Returns:
-        dict: Non-relational data as dictionary.
-    """
-    with open(Path(__cwd__).joinpath(database), "r") as file:
-        return json.loads(file.read())
+from ai2business.datasets.data import database
 
 
 def stock_market(indices: str = "DOWJONES") -> dict:
@@ -29,6 +12,7 @@ def stock_market(indices: str = "DOWJONES") -> dict:
         dict: Collection of the indices of the current stock market.
     """
     try:
-        return load_json("stock_market_index.json")[indices.upper()]
+        return database.StockMarket.__dict__[indices.lower()]
     except KeyError as exc:
         print(f"ERROR: {exc} -> Indices is not listed in the databse!")
+        return {}
