ead01591e3fea8c82b5f2999b7f6ecd201bd7edb
Sourcery AI <>
2020-12-15 18:16:17 +0000

    'Refactored by Sourcery'

--- docs/tutobooks.py
+++ docs/tutobooks.py
@@ -165,9 +165,7 @@ def py_to_nb(py_path, nb_path, fill_outputs=True):
             else:
                 cell["metadata"] = {"colab_type": "text"}
             cells.append(cell)
-    notebook = {}
-    for key in NB_BASE.keys():
-        notebook[key] = NB_BASE[key]
+    notebook = {key: NB_BASE[key] for key in NB_BASE.keys()}
     notebook["metadata"]["colab"]["name"] = str(py_path).split("/")[-1][:-3]
     notebook["cells"] = cells
     if loc > MAX_LOC:
@@ -396,10 +394,7 @@ def _get_next_script_element(py):
         else:
             elines.append(line)
 
-    if etype == "markdown":
-        py = "\n".join(lines[i + 1 :])
-    else:
-        py = "\n".join(lines[i:])
+    py = "\n".join(lines[i + 1 :]) if etype == "markdown" else "\n".join(lines[i:])
     e = "\n".join(elines)
 
     return e, etype, py, tag
@@ -435,10 +430,11 @@ def _make_output_code_blocks(md):
     is_inside_backticks = False
 
     def is_output_line(line, prev_line, output_lines):
-        if line.startswith("    ") and len(line) >= 5:
-            if output_lines or (lines[i - 1].strip() == "" and line.strip()):
-                return True
-        return False
+        return bool(
+            line.startswith("    ")
+            and len(line) >= 5
+            and (output_lines or (lines[i - 1].strip() == "" and line.strip()))
+        )
 
     def flush(output_lines, final_lines):
         final_lines.append('<div class="k-default-codeblock">')
@@ -522,16 +518,6 @@ if __name__ == "__main__":
     source = sys.argv[2]
     target = sys.argv[3]
 
-    if cmd == "py2nb":
-        if not source.endswith(".py"):
-            raise ValueError(
-                "The source filename should be a Python file. Got:", source
-            )
-        if not target.endswith(".ipynb"):
-            raise ValueError(
-                "The target filename should be a notebook file. Got:", target
-            )
-        py_to_nb(source, target)
     if cmd == "nb2py":
         if not source.endswith(".ipynb"):
             raise ValueError(
@@ -542,3 +528,13 @@ if __name__ == "__main__":
                 "The target filename should be a Python file. Got:", target
             )
         nb_to_py(source, target)
+    elif cmd == "py2nb":
+        if not source.endswith(".py"):
+            raise ValueError(
+                "The source filename should be a Python file. Got:", source
+            )
+        if not target.endswith(".ipynb"):
+            raise ValueError(
+                "The target filename should be a notebook file. Got:", target
+            )
+        py_to_nb(source, target)
