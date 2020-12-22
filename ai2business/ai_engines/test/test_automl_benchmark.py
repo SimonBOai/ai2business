63a901442b21b50e6ac6f04405898c2738bd8c2b
Anselm Hahn <Anselm.Hahn@gmail.com>
2020-12-22 18:05:28 +0100

    Update coverage test

--- ai2business/ai_engines/test/test_automl_benchmark.py
+++ ai2business/ai_engines/test/test_automl_benchmark.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
 
 from ai2business.ai_engines import automl_neural_network as an
 
@@ -33,3 +35,31 @@ def test_runtime_dataclassifier():
     assert context.return_automl["model"] != None
     assert isinstance(context.return_automl["prediction"], np.ndarray)
     assert isinstance(context.return_automl["evaluation"], list)
+
+
+def test_runtime_dataregression():
+
+    data = fetch_california_housing()
+    x_train, y_train, x_test, y_test = train_test_split(
+        data.data,
+        data.target,
+        test_size=0.33,
+        random_state=42,
+    )
+    context = an.AutoMLPipeline(an.DataRegression(max_trials=4))
+    context.run_automl()
+    context.train = an.AutoMLFit(x_train, y_train, batch_size=32, epochs=100)
+    context.run_automl()
+    context.train = an.AutoMLEvaluate(x_test, y_test, batch_size=32)
+    context.run_automl()
+    context.train = an.AutoMLPredict(x_train, batch_size=32)
+    context.run_automl()
+    assert context.return_automl["model"] != None
+    assert isinstance(context.return_automl["prediction"], np.ndarray)
+    assert isinstance(context.return_automl["evaluation"], list)
+
+
+def test_return_train():
+
+    context = an.AutoMLPipeline(an.DataRegression(max_trials=4))
+    assert context.train == an.AutoMLPipeline(an.DataRegression(max_trials=4))
