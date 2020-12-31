5350404375d7144358b8cb1f0e774cbb3b34b6ae
Anselm Hahn <Anselm.Hahn@gmail.com>
2020-12-31 18:56:39 +0100

    Increase coverage rate

--- ai2business/visualization/test/test_data_visualization.py
+++ ai2business/visualization/test/test_data_visualization.py
@@ -9,19 +9,20 @@ import seaborn as sns
 from ai2business.visualization import data_visualization as dav
 
 
+df_nan = pd.DataFrame(
+    np.random.randn(5, 3),
+    index=["a", "c", "e", "f", "h"],
+    columns=["one", "two", "three"],
+)
+df_nan["four"] = "bar"
+df_nan["five"] = df_nan["one"] > 0
+df_nan = df_nan.reindex(["a", "b", "c", "d", "e", "f", "g", "h"])
+
+
 def test_visual_missing_data():
-
-    df = pd.DataFrame(
-        np.random.randn(5, 3),
-        index=["a", "c", "e", "f", "h"],
-        columns=["one", "two", "three"],
-    )
-    df["four"] = "bar"
-    df["five"] = df["one"] > 0
-    df = df.reindex(["a", "b", "c", "d", "e", "f", "g", "h"])
-
+    # Test visualization of missing data
     data = dav.DataVisualization()
-    builder = dav.DesignerDataVisualization(df)
+    builder = dav.DesignerDataVisualization(df_nan)
     data.builder = builder
     data.visual_missing_data()
     result = builder.data_figure.return_product
@@ -29,3 +30,16 @@ def test_visual_missing_data():
         result[key].get_figure().savefig(Path(f"./test_visual_missing_data_{key}.png"))
 
     assert len(list(Path(".").glob("test_visual_missing_data_*.png"))) == i + 1
+
+
+def test_list_product_parts():
+    # Test return of prducts
+    data = dav.DataVisualization()
+    builder = dav.DesignerDataVisualization(df_nan)
+    data.builder = builder
+    data.visual_missing_data()
+    result = builder.data_figure.list_product_parts
+    assert (
+        result
+        == "Product parts: get_nullity_matrix, get_nullity_bar, get_nullity_heatmap, get_nullity_dendrogram"
+    )
